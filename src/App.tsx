604cb77b9c0dddb8336e874fb60dcfb1aa0724ff
dkozak <dkozak94@gmail.com>
2019-05-23 09:20:48 +0100

    page desing, WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
-import React, {useState} from "react";
-// @ts-ignore
-import Graph from "react-graph-vis";
-import {graph2vis} from "./graph2vis";
-import {NodeEditForm} from "./NodeEditForm";
-
-const initialNodes = [
-    {id: 0, name: 'x', neighbours: [1]},
-    {id: 1, name: 'y', neighbours: [2]},
-    {id: 2, name: 'z', neighbours: []},
-]
+import React from "react";
+import "./App.css"
 
 export const App = () => {
-    const [nodes, setNodes] = useState(initialNodes)
-    const visGraph = graph2vis(nodes)
 
-    const options = {
-        edges: {
-            color: "#000000"
-        }
-    };
-
-    const events = {
-        select: function (event: any) {
-            const {nodes, edges} = event;
-        }
-    }
-
-    return <div style={{width: '100%', height: '100%'}}>
-        <NodeEditForm nodes={nodes} setNodes={setNodes}/>
-        <Graph graph={visGraph} options={options} events={events}/>
+    return <div className="App">
+        <Header/>
+        <div className="Central">
+            <Navbar/>
+            <Content/>
+        </div>
+        <Footer/>
     </div>
 };
+
+export const Header = () => {
+
+    return <div className="Header">
+        <p>Graph viz</p>
+    </div>
+}
+
+export const Navbar = () => {
+    return <div>
+        <h1>navbar</h1>
+    </div>
+}
+
+export const Content = () => {
+    return <div className="Content">
+        <h1>content</h1>
+    </div>
+}
+
+export const Footer = () => {
+    return <div className="Footer">
+        <p>Graph viz @ d-kozak</p>
+    </div>
+}
---
+++ src/SimpleGraph.tsx
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+// @ts-ignore
+import Graph from "react-graph-vis";
+import {graph2vis} from "./graph2vis";
+import {NodeEditForm} from "./NodeEditForm";
+
+const initialNodes = [
+    {id: 0, name: 'x', neighbours: [1]},
+    {id: 1, name: 'y', neighbours: [2]},
+    {id: 2, name: 'z', neighbours: []},
+]
+
+export const SimpleGraph = () => {
+    const [nodes, setNodes] = useState(initialNodes)
+    const visGraph = graph2vis(nodes)
+
+    const options = {
+        edges: {
+            color: "#000000"
+        }
+    };
+
+    const events = {
+        select: function (event: any) {
+            const {nodes, edges} = event;
+        }
+    }
+
+    return <div style={{width: '100%', height: '100%'}}>
+        <NodeEditForm nodes={nodes} setNodes={setNodes}/>
+        <Graph graph={visGraph} options={options} events={events}/>
+    </div>
+};
